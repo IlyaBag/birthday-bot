7c1097e47dd42dc3f5c96f8a9433a557a599c3a3
Ilya Bagrov <ilya.b.only@gmail.com>
2025-01-21 01:19:48 +0300

    Initial commit

---
+++ bot/main.py
@@ -0,0 +1,27 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from decouple import config
+
+
+bot = Bot(
+    token=config('TOKEN'),
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+dp = Dispatcher()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s | %(levelname)s | %(name)s | %(message)s'
+)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
