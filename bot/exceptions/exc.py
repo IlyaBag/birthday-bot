99498efa9cec8089f4de2d09f23a2fffdb1262c0
Ilya Bagrov <ilya.b.only@gmail.com>
2025-01-23 01:22:11 +0300

    Add storage path validation. Add exceptions pckg

---
+++ bot/exceptions/exc.py
@@ -0,0 +1,2 @@
+class StoragePathError(Exception):
+    pass
--- bot/repo/json_storage.py
+++ bot/repo/json_storage.py
@@ -1,6 +1,9 @@
 import json
+import os
 
 from decouple import config  # type: ignore
+
+from exceptions.exc import StoragePathError
 
 
 NO_BIRTHDAY_PERSON_MSG = 'В этот день у нас нет именинников'
@@ -8,6 +11,8 @@ NO_BIRTHDAY_PERSON_MSG = 'В этот день у нас нет именинни
 
 def get_all_birthdays() -> dict[str, str]:
     storage_path = config('STORAGE_PATH', default='birthdays.json')
+    if not os.path.exists(storage_path):
+        raise StoragePathError(f'Storage path "{storage_path}" does not exist.')
     with open(storage_path, 'r') as f:
         birthdays = json.load(f)
     return birthdays
