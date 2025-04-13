7bec3ef5e721757f87453a2f7d4285aabbbe5cd2
Ilya Bagrov <ilya.b.only@gmail.com>
2025-04-13 15:37:45 +0300

    Remove the unused kludge function

--- bot/utils/users.py
+++ bot/utils/users.py
@@ -1,8 +1,3 @@
-from datetime import datetime
-
-from aiogram.types import User
-from decouple import config  # type: ignore
-
 from repo.json_storage import get_all_users
 
 
@@ -10,9 +5,3 @@ def get_all_users_ids() -> list[int]:
     """Return a list of integers representing user IDs."""
     users = get_all_users()
     return [int(user['tg_id']) for user in users]
-
-def temporary_save_new_user(user: User) -> None:
-    '''Временная функция-костыль. Сохраняет данные пользователя в отдельный файл.'''
-    with open('new_users.txt', 'a') as f:
-        f.write(f'{datetime.now()}\n')
-        f.write(f'{user}\n')
