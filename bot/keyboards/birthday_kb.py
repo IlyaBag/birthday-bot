5892491f5619855b122ac44d560be27bd87abaf4
Ilya Bagrov <ilya.b.only@gmail.com>
2025-04-25 00:25:21 +0300

    Update main keyboard view

--- bot/keyboards/birthday_kb.py
+++ bot/keyboards/birthday_kb.py
@@ -5,12 +5,12 @@ from create_bot import admins
 
 def main_menu_kb(user_id: int) -> ReplyKeyboardMarkup:
     keyboard = ReplyKeyboardBuilder()
-    keyboard.button(text='Сегодня')
-    keyboard.button(text='Ближайший')
+    keyboard.button(text='🎉 Сегодня')
+    keyboard.button(text='⏳ Ближайший')
     keyboard.button(text='🔎 Поиск')
     if user_id in admins:
-        keyboard.button(text='temp')
         keyboard.button(text='logs')
+        keyboard.button(text='temp')
     keyboard.adjust(2, 1, 2)
     return keyboard.as_markup(resize_keyboard=True,
                               input_field_placeholder='Нажми на кнопку')
